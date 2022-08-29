ef85a9b5c8d1cf38343a86722b149776cb25a98f
Valentin Pimenov <valker.rus@gmail.com>
2022-08-29 21:27:31 +0300

    first fail with repeated-field

--- AutoFixtureWithGrpc/Program.cs
+++ AutoFixtureWithGrpc/Program.cs
@@ -3,5 +3,6 @@ using AutoFixtureWithGrpc;
 
 var fixture = new Fixture();
 
+fixture.Behaviors.Add(new ReadonlyCollectionPropertiesBehavior());
 var message = fixture.Create<HelloRequest>();
-Console.WriteLine(message);
+Console.WriteLine(message.LuckyNumbers.Count);
