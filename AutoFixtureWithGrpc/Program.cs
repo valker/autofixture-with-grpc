7295cf5bf03a830a3f6dd9206b2ec10e8aeb5f94
Valentin Pimenov <valker.rus@gmail.com>
2022-08-29 20:37:45 +0300

    initial commit

---
+++ AutoFixtureWithGrpc/Program.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+
+var fixture = new Fixture();
+
+var intValue = fixture.Create<int>();
+Console.WriteLine(intValue);
+
+var complexType = fixture.Create<ComplexType>();
+Console.WriteLine(complexType);
+
+var collection = fixture.Create<List<ComplexType>>();
+Console.WriteLine(string.Join(", ", collection));
+
+record ComplexType(int IntValue, string StringValue);
