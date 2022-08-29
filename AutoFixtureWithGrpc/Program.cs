363ae5ec9bfcb3f31c130cbdf60b426b93832654
Valentin Pimenov <valker.rus@gmail.com>
2022-08-29 20:54:17 +0300

    add grpc

--- AutoFixtureWithGrpc/Program.cs
+++ AutoFixtureWithGrpc/Program.cs
@@ -1,14 +1,7 @@
 ﻿using AutoFixture;
+using AutoFixtureWithGrpc;
 
 var fixture = new Fixture();
 
-var intValue = fixture.Create<int>();
-Console.WriteLine(intValue);
-
-var complexType = fixture.Create<ComplexType>();
-Console.WriteLine(complexType);
-
-var collection = fixture.Create<List<ComplexType>>();
-Console.WriteLine(string.Join(", ", collection));
-
-record ComplexType(int IntValue, string StringValue);
+var message = fixture.Create<HelloRequest>();
+Console.WriteLine(message);
